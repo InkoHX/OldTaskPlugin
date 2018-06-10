13645fb434b6cc8caf18f2379dfea3c2be5834d5
InkoHX <inkohx@gmail.com>
2018-06-10 18:03:25 +0900

    Upload

---
+++ src/oldtask/Main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PCink
+ * Date: 2018/06/10
+ * Time: 17:39
+ */
+
+namespace oldtask;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onEnable()
+    {
+        $this->getLogger()->info("OldTaskを読み込みました。\n使い方は https://github.com/InkoHX/OldTaskPlugin を見て下さい。");
+    }
+}
